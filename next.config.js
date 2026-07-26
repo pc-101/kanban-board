a7b8191584a0af759704b7e1298d89d41fcdc0e4
Peter Chea <longkchea@gmail.com>
2026-07-25 18:41:57 -0700

    Fix failed compilation after implementation changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-/** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = {
-  output: "export",
-  assetPrefix: isGithubActions ? `/${repo}` : "",
-  basePath: isGithubActions ? `/${repo}` : "",
-  images: { unoptimized: true },
+/** @type {(phase: string) => import('next').NextConfig} */
+module.exports = (phase) => {
+  const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+  const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+  return {
+    output: "export",
+    distDir: isDev ? ".next-dev" : ".next",
+    assetPrefix: isGithubActions ? `/${repo}` : "",
+    basePath: isGithubActions ? `/${repo}` : "",
+    images: { unoptimized: true },
+  };
 };
