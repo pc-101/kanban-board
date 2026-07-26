0bb702b333e62e6299ad143d97d7dfc7e6aa6435
Peter Chea <longkchea@gmail.com>
2026-07-25 18:40:54 -0700

    Change typeface to Satoshi

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="preconnect" href="https://api.fontshare.com" />
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=satoshi@400,500,600,700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <ThemeProvider>
           <Header />
